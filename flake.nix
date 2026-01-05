2cead52908bdee2e3a38ee53db2fe668c4902613
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-01-05 14:29:12 -0800

    feat: init commits

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+      systems = ["x86_64-linux"];
+      perSystem = {system, ...}: let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [
+          ];
+        };
+      in {
+        formatter.default = pkgs.alejandra;
+        devShells.default = pkgs.mkShell {
+          name = "taskbane";
+
+          buildInputs = with pkgs; [
+            cargo
+            cargo-generate
+            rustc
+            rustfmt
+          ];
+
+          shellHook = ''
+            function menu () {
+              echo
+              echo -e "\033[1;34m>==> ️  '$name' shell\n\033[0m"
+              just --list
+              echo
+              echo "(Run 'just --list' to display this menu again)"
+              echo
+            }
+
+            menu
+            just --list
+          '';
+        };
+      };
+      flake = {};
+    };
+}
