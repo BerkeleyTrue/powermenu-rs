f0079766c03086634c303ed74cbd2d7d140d5c93
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-01-08 21:32:58 -0800

    feat(main): some speedups

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ mod app;
 mod dead_internet;
 
 use clap::Parser;
-use gtk4::{
-    CssProvider, STYLE_PROVIDER_PRIORITY_APPLICATION, STYLE_PROVIDER_PRIORITY_USER, gdk::Display,
-};
+use gtk4::{CssProvider, STYLE_PROVIDER_PRIORITY_APPLICATION, gdk::Display};
 use relm4::{RelmApp, gtk};
 use tracing::{Level, debug};
 use tracing_subscriber::FmtSubscriber;
@@ -19,23 +17,11 @@ fn load_css() {
     let default_provider = CssProvider::new();
     default_provider.load_from_resource("/com/bt/powermenu-rs/style.css");
 
-    let user_provider = CssProvider::new();
-    if let Some(config_dir) = dirs::config_dir() {
-        let user_css = config_dir.join("powermenu/style.css");
-        if user_css.exists() {
-            user_provider.load_from_path(user_css);
-        }
-    }
     let display = Display::default().expect("Cannot get default display");
     gtk::style_context_add_provider_for_display(
         &display,
         &default_provider,
         STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
-    gtk::style_context_add_provider_for_display(
-        &display,
-        &user_provider,
-        STYLE_PROVIDER_PRIORITY_USER,
     );
 }
 
@@ -61,12 +47,8 @@ fn get_user() -> String {
 }
 
 fn get_host() -> Option<String> {
-    std::process::Command::new("uname")
-        .arg("-n")
-        .output()
-        .map(|out| out.stdout)
-        .map(|out_bytes| String::from_utf8_lossy(&out_bytes).to_string())
-        .inspect_err(|err| debug!("host err: {err:?}"))
+    std::fs::read_to_string("/proc/sys/kernel/hostname")
+        .map(|s| s.trim().to_string())
         .ok()
 }
 
